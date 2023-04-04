77265982ad391b0d85659aba268d7907792c2379
NelsonMALGAS <zmalgas69@gmail.com>
2023-04-04 11:28:38 +0200

    modified script.js

--- src/Challenge1-JS/scripts.js
+++ src/Challenge1-JS/scripts.js
@@ -2,9 +2,12 @@ const primaryPhone = 'O748105141';
 const secondaryPhone = '0219131568';
 
 // Only change below this line
+// function containsOnlyDigits(){
+//     return /^\[0-9]/.test();
+// }
 
-const primaryValid = typeof primaryPhone === 'string' &&  /^\d+$/.test(primaryPhone)  
-const secondaryValid = typeof secondaryPhone === 'string' && /^\d+$/.test(secondaryPhone)  
+const primaryValid = typeof primaryPhone === 'string' && /^[0-9]/.test(primaryPhone)  
+const secondaryValid = typeof secondaryPhone === 'string' && /^[0-9]/.test(secondaryPhone)  
 
 console.log('Primary phone is valid numerical string:', primaryValid);
 console.log('Secondary phone is valid numerical string:', secondaryValid );
